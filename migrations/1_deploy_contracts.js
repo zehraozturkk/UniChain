dff8e04b13e8c154a0eb23358143581e1c9ab649
Beyza Evcen <beyzaevcen37@icloud.com>
2024-01-25 12:37:23 +0300

    Login page edited

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -3,7 +3,7 @@ const TokenExchange = artifacts.require("TokenExchange");
 module.exports = function (deployer) {
   const newName = "NewTokenName";
   const newSymbol = "NTN";
-  const tokenAddress = "0x5F3c884e8D9096271dEDe693e4A2EC6647dB86B9"; // ERC-20 token adresini gerçek bir değerle değiştirin
+  const tokenAddress = "0x5F3c884e8D9096271dEDe693e4A2EC6647dB86B9"; 
 
   deployer.deploy(TokenExchange, newName, newSymbol, tokenAddress);
 };
